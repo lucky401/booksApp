b6f21d422f4ce891315077f7ad7a9a6f64eed3eb
Lucky Dewa Satria <luckydewa123@gmail.com>
2020-11-10 21:04:22 +0700

    Initial commit

---
+++ app/src/main/java/com/ldsoft/booksapp/ItemData.java
@@ -0,0 +1,9 @@
+package com.ldsoft.booksapp;
+
+public class ItemData {
+    public String itemTitle;
+    public String itemAuthor;
+    public String itemImage;
+    public String itemDescription;
+
+}
